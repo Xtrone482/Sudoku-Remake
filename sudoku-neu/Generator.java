4bb057921f91a2c554f4cf8ebc3ae32d88ebbb82
Lenovo <xtrone.hd@gmail.com>
2024-11-29 14:06:10 +0100

    Refresh

--- sudoku-neu/Generator.java
+++ sudoku-neu/Generator.java
@@ -17,10 +17,10 @@ public class Generator {
 		
 		stack = stackPusher(stack);
 		
-		int maximum = stackCounter(stack);
+		int maximum = stackCounter(stack) - 1;
 		
-		int x = (int) (Math.random() * maximum;
-		
+		int x = (int) (Math.random() * maximum) + 1;
+		System.out.println("Zufallswert " + x);
 		return x;
 	}
 	
@@ -29,21 +29,19 @@ public class Generator {
 		int speicher = 0; 
 		
 		if (negativ == true) {
-			for (int i = target; i > stack.length; i--) {
-				speicher = stack[i-1];
-				stack[i-1] = stack[i];
+			for (int i = target; i > 0; i--) {
+				speicher = stack[i];
+				stack[i] = stack[i-1];
+				stack[i-1] = speicher;
+			}
+		} else {
+			for (int i = target; i < stack.length - 1; i++) {
+				speicher = stack[i+1];
+				stack[i+1] = stack[i];
 				stack[i] = speicher;
 			}
-			
-			return stack;
 		}
 		
-		for (int i = target; i < stack.length; i++) {
-			speicher = stack[i];
-			stack[i] = stack[i+1];
-			stack[i+1] = speicher;
-		}
-
 		return stack;
 	}
 	
@@ -51,7 +49,7 @@ public class Generator {
 		
 		for (int i = 0; i < stack.length; i++) {
 			if (stack[i] == 0) {
-				stackMover(stack , stack[i] , false);
+				stackMover(stack , i , false);
 			}
 		}
 		
@@ -74,6 +72,7 @@ public class Generator {
 	static int[][] generate(int menge) {		// Menge muss {n E N: root(n) E N} sein.
 		
 		int[][][] tempFeld = new int[menge][menge][menge];
+		int Random;
 		
 		// Initialisierung
 		for (int i = 0; i < menge; i++) {
@@ -85,13 +84,20 @@ public class Generator {
 		}
 		
 		// Main
+		
+		/*
 		int[] tempStack = new int[menge];
+		for (int i = 0; i < tempStack.length; i++) {
+			System.out.println(tempStack[i]);
+		}
+		*/
 		
 		for (int i = 0; i < menge; i++) {
 			for (int j = 0; j < menge; j++) {
-				stackMover(tempFeld[i][j] , stackRandom(tempFeld[i][j]) , true);
+				Random = stackRandom(tempFeld[i][j]);
+				tempFeld[i][j] = stackMover(tempFeld[i][j] , Random , true);
 				tempFeld = generatorFehlersuche(tempFeld , i , j);
-				}
+				
 			} 
 		}
 		
@@ -101,7 +107,7 @@ public class Generator {
 		
 		for (int i = 0; i < menge; i++) {
 			for (int j = 0; j < menge; j++) {
-				finalFeld[i][j] = tempFeld[i][j][0]
+				finalFeld[i][j] = tempFeld[i][j][0];
 			} 
 		}
 		
@@ -110,25 +116,27 @@ public class Generator {
 	
 	static int[][][] generatorFehlersuche(int[][][] feld , int indikatorX , int indikatorY) {
 		
-		for (int i = feld.length; i > 1; i--) {
+		int indikatorWert = feld[indikatorX][indikatorY][0] - 1;
+		System.out.println("Pradikator  " + feld[indikatorX][indikatorY][0]);
+		System.out.println("Indikator   " + indikatorWert);
+		
+		for (int i = 1; i < feld.length; i++) {
+			System.out.println(feld[indikatorX][indikatorY][i]);
 			feld[indikatorX][indikatorY][i] = 0;
 		}
-		int indikatorWert = feld[indikatorX][indikatorY][0] - 1;
 		
 		// Reihe[]
 		for (int i = 0; i < feld.length; i++) {
-			if (i == indikatorX) {
-				i++;
+			if (i != indikatorX) {
+				feld[i][indikatorY][indikatorWert] = 0;
 			}
-			feld[i][indikatorY][indikatorWert] = 0;
 		}
 		
 		// Spalte[][]
 		for (int i = 0; i < feld.length; i++) {
-			if (i == indikatorY) {
-				i++;
+			if (i != indikatorY) {
+				feld[indikatorX][i][indikatorWert] = 0;
 			}
-			feld[indikatorX][i][indikatorWert] = 0;
 		}
 		
 		// Block-System
@@ -140,13 +148,13 @@ public class Generator {
 		// Block-Finder
 		while (gate != true) {
 			blockX++;
-			if (indikatorX < dimension * blockX)) {
+			if (indikatorX < dimension * blockX) {
 				gate = true;
 			}
 		}
 		while (gate != false) {
 			blockY++;
-			if (indikatorY < dimension * blockY)) {
+			if (indikatorY < dimension * blockY) {
 				gate = false;
 			}
 		}
@@ -154,11 +162,9 @@ public class Generator {
 		// Block-Cleaner
 		for (int i = (blockX - 1) * dimension; i < blockX * dimension; i++) {
 			for (int j = (blockY - 1) * dimension; j < blockY * dimension; j++) {
-				
-				if (i == indikatorX) i++;
-				if (j == indikatorY) j++;
-				
-				feld[i][j][indikatorWert] = 0;
+				if (!(i == indikatorX && j == indikatorY)) {
+					feld[i][j][indikatorWert] = 0;
+				}
 			}
 		}
 		
@@ -169,6 +175,11 @@ public class Generator {
 	
 	public static void main(String[] args) {
 		feldPrinter(generate(9));
+		int[] test = {1,2,3,4,5,0,7,8,9};
+		test = stackMover(test , 5 , true);
+		for (int i = 0; i < test.length; i++) {
+			//System.out.println(test[i]);
+		}
 	}
 	
 }
